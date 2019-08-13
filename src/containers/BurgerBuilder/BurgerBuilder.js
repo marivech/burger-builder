05e4bcdf5d383b6fc2f6e20da0ce0274ae8aad7e
marivech <maria.vechkanova@gmail.com>
2019-08-13 16:13:56 +0400

    Add basic components

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux/Aux';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger builder</div>
+                <div>Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
