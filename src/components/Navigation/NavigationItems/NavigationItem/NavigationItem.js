11c78c309b14c16d2008fe26a720983554e8c942
marivech <maria.vechkanova@gmail.com>
2019-09-28 00:09:26 +0400

    Refactor navigation

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import classes from './NavigationItem.module.css';
+import { NavLink} from 'react-router-dom';
 
 const navigationItem = props => (
-    <li className={classes.NavigationItem}>
-        <a 
-            href={props.link}
-            className={props.active ? 'active' : null}>{props.children}</a>
+    <li className={ classes.NavigationItem }>
+        <NavLink 
+            to={props.link}
+            exact
+            activeClassName={ classes.active }>{ props.children }</NavLink>
     </li>
 );
 export default navigationItem;
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,8 +4,8 @@ import classes from './NavigationItems.module.css';
 
 const navigationItems = props => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem link='/' active>Burger builder</NavigationItem>
-        <NavigationItem link='/'>Checkout</NavigationItem>
+        <NavigationItem link='/'>Burger builder</NavigationItem>
+        <NavigationItem link='/orders'>Orders</NavigationItem>
     </ul>
 );
 export default navigationItems;
